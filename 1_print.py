3f6ff79d60c3ac691904d0b402f6d2ec431896f9
theeasylearn <theeasylearn@gmail.com>
2025-07-01 20:44:21 +0530

    first commit

---
+++ 1_print.py
@@ -0,0 +1,9 @@
+'''
+    1st program in python 
+    use of print statement 
+    author : Ankit Patel
+'''
+print('Hello world')
+print("this is 2nd line")
+print("""Ankit Patel 
+105, Eva surbhi, Opp Aksharwadi temple, Waghawadi road, Bhavnagar - 364001""")
