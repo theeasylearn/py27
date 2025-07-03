56e6ea8fe13b5f856e3b6519bf68ea2c3eda8f3e
theeasylearn <theeasylearn@gmail.com>
2025-07-03 20:37:35 +0530

    string

---
+++ 3_string.py
@@ -0,0 +1,17 @@
+#string concept in python 
+line = "The easylearn academy"
+print(line) 
+print(line[0]) #it will display letter at 0th position 
+print(line[0:3]) #it will display 1st 3 letter (0,1,2) not 3
+print(line[4:14]) #it will display letter 5 to 17
+print(line[4:]) #it will display all letter from 4th position onwards
+print(line*3) #print variable line 3 times
+city = "bhavnagar"
+print(line + " " + city)
+number = 100
+# print(number + city) #error because we can not concatinate different type of vriable python 
+print(number,200)
+line = 'T.E.L'
+print(line)
+# line[0] = 'x' it is error as string's part can not be changed
+print('good bye')
