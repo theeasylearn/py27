1ea6d5a32c2a0a10f9ba764c4caa92de77fad01e
theeasylearn <theeasylearn@gmail.com>
2025-07-16 20:33:05 +0530

    decision making statement

---
+++ 13_if_decision.py
@@ -0,0 +1,13 @@
+'''
+    write a program to accept 24 hours format time from user and convert it into 12 hours format time and display it 
+    time : 23 output 11:00 PM 
+    time : 09 output 09:00 AM 
+'''
+time = int(input("Enter hours (between 1 to 24)"))
+if time<13:
+    print(f"{time}:00 AM ")
+if time>=13:
+    time = time - 12 
+    print(f"{time}:00 PM ")
+    
+print("Good Bye.")
