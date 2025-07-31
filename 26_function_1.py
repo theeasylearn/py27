86822598063e3c96f3475f369aef604568388631
theeasylearn <theeasylearn@gmail.com>
2025-07-31 20:39:29 +0530

    functions

--- 26_function_1.py
+++ 26_function_1.py
@@ -1,12 +1,29 @@
 #create function that return total minutes from given hours and given minutes 
+
+# without return value without argument 
+def printLine():
+    print("_"*110)
+    
+#without return value with argument 
+def printLetter(letter,count):
+    print(letter*count)
 # with return value with argument function
+
+def getMinutes():
+    minutes = 60
+    return minutes
+
 def toMinutes(hours,minutes):
     TotalMinutes = (hours * 60) + minutes
     return TotalMinutes
 
-
+printLine()
 hours = int(input("Enter hours"))
 minutes = int(input("Enter minutes"))
+printLine()
+
 
 TotalMinutes = toMinutes(hours,minutes)
+printLetter('*',100)
 print(f"total minutes = {TotalMinutes}")
+printLetter('^',110)
---
+++ 27_function_2.py
@@ -0,0 +1,23 @@
+def getTotalSeconds(hours,minutes=0,seconds=0):
+    print(f"hours = {hours} minutes = {minutes} seconds = {seconds}")
+    #local variable (variable declared inside function is called local variable)
+    totalSeconds = hours * 60 * 60
+    totalSeconds += minutes * 60
+    totalSeconds += seconds
+    return totalSeconds
+
+h = 10
+m = 30 
+s = 15
+totalSeconds = getTotalSeconds(h,m,s)
+print(f"total seconds = {totalSeconds}")
+
+totalSeconds = getTotalSeconds(h,m)
+print(f"total seconds = {totalSeconds}")
+
+totalSeconds = getTotalSeconds(h)
+print(f"total seconds = {totalSeconds}")
+
+#keywords arguments
+totalSeconds = getTotalSeconds(minutes=m,seconds=s,hours=h)
+print(f"total seconds = {totalSeconds}")
