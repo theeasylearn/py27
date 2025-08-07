a357bbd9e3914c7cd11effdc3eb2849e701a1adf
theeasylearn <theeasylearn@gmail.com>
2025-08-07 20:44:18 +0530

    recursion

---
+++ 33_resursion_1.py
@@ -0,0 +1,11 @@
+# write a program to convert decimal number into binary 
+
+def toBinary(number):
+    if number>0:
+        reminder = number % 2
+        number = number // 2
+        toBinary(number) #recursion
+        print(reminder,end=' ')
+number = int(input("Enter number"))
+toBinary(number)
+
